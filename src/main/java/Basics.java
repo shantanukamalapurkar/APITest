6017b521cf5ced1129b236b2244f702b98ebcda9
shantanu kamalapurkar <shantanu.kamalapurkar@gmail.com>
2022-05-05 07:58:16 -0400

    First Commit

---
+++ src/main/java/Basics.java
@@ -0,0 +1,26 @@
+import files.payload;
+import io.restassured.RestAssured;
+
+import static io.restassured.RestAssured.given;
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
+
+//This is first commit
+public class Basics {
+    public static void main(String args[]){
+        RestAssured.baseURI  = "https://rahulshettyacademy.com";
+        given().queryParam("key","qaclick123")
+                .header("Content-Type","application/json")
+                .body(payload.AddPlace()).when().post("maps/api/place/add/json")
+                .then().log().all().assertThat().statusCode(200);
+                //.header("server","Apache/2.4.41 (Ubuntu)");
+       /* given().queryParam("key","qaclick123").queryParam("place_id","0b4cac7d1555df427dd981a6483e457b")
+                .when().get("maps/api/place/get/json")
+                .then().assertThat().log().all().statusCode(200);*/
+        int a;
+        a=10;
+        int b=20;
+        int c = a+b;
+        System.out.println("C is "+c);
+    }
+}
---
+++ src/main/java/files/payload.java
@@ -0,0 +1,24 @@
+package files;
+
+public class payload {
+
+    public static String AddPlace(){
+
+        return "{\\n\" +\n" +
+                "                        \"  \\\"location\\\": {\\n\" +\n" +
+                "                        \"    \\\"lat\\\": -38.383494,\\n\" +\n" +
+                "                        \"    \\\"lng\\\": 33.427362\\n\" +\n" +
+                "                        \"  },\\n\" +\n" +
+                "                        \"  \\\"accuracy\\\": 50,\\n\" +\n" +
+                "                        \"  \\\"name\\\": \\\"Frontline house\\\",\\n\" +\n" +
+                "                        \"  \\\"phone_number\\\": \\\"(+91) 983 893 3937\\\",\\n\" +\n" +
+                "                        \"  \\\"address\\\": \\\"29, side layout, cohen 09\\\",\\n\" +\n" +
+                "                        \"  \\\"types\\\": [\\n\" +\n" +
+                "                        \"    \\\"shoe park\\\",\\n\" +\n" +
+                "                        \"    \\\"shop\\\"\\n\" +\n" +
+                "                        \"  ],\\n\" +\n" +
+                "                        \"  \\\"website\\\": \\\"https://rahulshettyacademy.com\\\",\\n\" +\n" +
+                "                        \"  \\\"language\\\": \\\"French-IN\\\"\\n\" +\n" +
+                "                        \"}\\n";
+    }
+}
